e3021600c72c02dd64bf6c99f6f765105b4ff3ee
Johan Nordström <nordstroem@gmail.com>
2022-01-04 13:32:18 +0100

    Better convergence

--- gym_main.py
+++ gym_main.py
@@ -53,22 +53,32 @@ def run():
     policy_net = DQN()
     target_net = DQN()
 
-    optimizer = torch.optim.Adam(policy_net.parameters(), lr=0.001)
+    def init_weights(m):
+        if isinstance(m, torch.nn.Linear):
+            torch.nn.init.kaiming_uniform_(m.weight)
+            torch.nn.init.zeros_(m.bias)
+
+    policy_net.apply(init_weights)
+    target_net.apply(init_weights)
+
+    optimizer = torch.optim.Adam(policy_net.parameters(), lr=0.0005)
 
     def to_input(s: np.ndarray, a: float):
         x = np.concatenate([s, [a]]).astype(np.float32)
         return torch.from_numpy(x)
 
-    memory = ReplayMemory(10000)
+    memory = ReplayMemory(5000)
 
     gamma = 0.999
     batch_size = 64
+    step_counter = 0
     total_rewards = []
     for episode in range(5000):
-        epsilon = max(0.05, 0.6 * (1 - episode / 300))
+        epsilon = max(0.1, 0.8 * (1 - episode / 500))
         current_state = env.reset()
         iteration = 0
         total_reward = 0
+        # epsilon = 0.05 + (0.9 - 0.05) * math.exp(-1. * episode / 200)
         while iteration < 500:
             iteration += 1
             # Epsilon greedy selection
@@ -80,44 +90,48 @@ def run():
                 next_action = np.random.randint(2)
 
             next_state, reward, done, _ = env.step(next_action)
+            step_counter += 1
             total_reward += 1
             next_transition = Transition(current_state, next_action, next_state, reward, done)
             memory.append(next_transition)
             current_state = next_state
+            moving_reward_average = np.mean(total_rewards[:-10]) if len(total_rewards) > 10 else 0.0
+            if step_counter % 1000 == 0 and step_counter > 0:
+                print(episode, moving_reward_average, epsilon)
+                target_net.load_state_dict(policy_net.state_dict())
+
+            if moving_reward_average > 60:
+                env.render()
 
             # Optimize q table
-            if len(memory) >= batch_size:
-                mini_batch = memory.random_batch(batch_size)
-                targets = []
-                inputs = []
-                for transition in mini_batch:
-                    if transition.is_terminal:
-                        target = transition.reward
-                    else:
-                        with torch.no_grad():
-                            next_q_set = [target_net(to_input(transition.next_state, ap)).item() for ap in [0.0, 1.0]]
-                        target = transition.reward + gamma * max(next_q_set)
+            if step_counter % 4 == 0:
+                if len(memory) >= batch_size:
+                    mini_batch = memory.random_batch(batch_size)
+                    targets = []
+                    inputs = []
+                    for transition in mini_batch:
+                        if transition.is_terminal:
+                            target = transition.reward
+                        else:
+                            with torch.no_grad():
+                                next_q_set = [target_net(to_input(transition.next_state, ap)).item() for ap in
+                                              [0.0, 1.0]]
+                            target = transition.reward + gamma * max(next_q_set)
 
-                    inputs.append(to_input(transition.state, transition.action))
-                    targets.append(torch.tensor(target))
+                        inputs.append(to_input(transition.state, transition.action))
+                        targets.append(torch.tensor(target))
 
-                y = torch.stack(targets)
-                x = torch.stack(inputs)
-                policy_net.zero_grad()
-                loss_fn = torch.nn.SmoothL1Loss()
-                loss = loss_fn(policy_net(x), y.unsqueeze(1))
-                loss.backward()
-                optimizer.step()
-
-            if episode > 250:
-                env.render()
+                    y = torch.stack(targets)
+                    x = torch.stack(inputs)
+                    policy_net.zero_grad()
+                    loss_fn = torch.nn.HuberLoss()
+                    loss = loss_fn(policy_net(x), y.unsqueeze(1))
+                    loss.backward()
+                    optimizer.step()
 
             if done:
                 break
         total_rewards.append(total_reward)
-        if episode % 10 == 0:
-            print(episode, np.mean(total_rewards[:-10]) if len(total_rewards) > 10 else 0.0, epsilon)
-            target_net.load_state_dict(policy_net.state_dict())
 
     env.close()
 
